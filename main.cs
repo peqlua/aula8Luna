05ecab94f8ef91550defeee16be316d4c2f647e2
Luna <128090844+peqlua@users.noreply.github.com>
2024-08-19 13:55:23 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+using static Helpers;
+class Program {
+  public static void Main (string[] args)  
+ String nome = Read("digite seu nome");
+ Write( nome + " se o seu nome e Pablo voçê é:");
+
+Write("Super ");
+
+  }
+}
